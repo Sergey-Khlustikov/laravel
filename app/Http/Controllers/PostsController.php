9106466b56ef320881c83dfaba84968720ac4a1f
serge <sergeykhlustkikov1@gmail.com>
2022-08-08 12:55:21 +0300

    feat

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -1,1 +1,87 @@
 <?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostsController extends Controller
+{
+    public function index()
+    {
+        return 'Posts';
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+
+    public function show($id)
+    {
+        return 'Post #' . $id;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    public function contact()
+    {
+        $people = ['Mykola', 'Oles', 'Oksana', 'Petya'];
+
+        return view('contact', compact('people'));
+    }
+
+    public function showPost($id)
+    {
+//        return view('post')->with('id', $id);
+        return view('post', compact('id'));
+    }
+}
