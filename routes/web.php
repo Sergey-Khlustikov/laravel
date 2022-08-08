795c882ea60cb64fd7865069b55171aba218eac3
serge <sergeykhlustkikov1@gmail.com>
2022-08-08 12:53:25 +0300

    feat

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,33 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/contact', [PostsController::class, 'contact']);
+
+Route::resource('posts', 'PostsController');
+
+Route::get('post/{id}', [PostsController::class, 'showPost']);
+
+//Route::get('/posts', [PostsController::class, 'index']);
+//Route::get('/posts/{id}', [PostsController::class, 'show']);
+
+
+//Route::get('/about', function () {
+//    return 'About page';
+//});
+//
+//Route::get('/contacts', function () {
+//    return 'Contacts page';
+//});
+//
+//// Dynamic param route
+//Route::get('/post/{id}', function ($id) {
+//    return 'This is post ' . $id;
+//});
+//
+//// Named Route
+//Route::get('admin/dashboard', array('as' => 'admin.dashboard', function() {
+//    $url = \route('admin.dashboard');
+//
+//    return 'Url is ' . $url;
+//}));
